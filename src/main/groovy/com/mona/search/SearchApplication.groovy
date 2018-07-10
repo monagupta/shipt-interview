7e885a2b2351550d17349387250e51f8000aaddb
Mona Gupta <monagupta43@gmail.com>
2018-07-10 18:55:06 -0500

    Create initial Spring Boot project with Groovy
    Includes dependencies:
      * spring boot starter
      * spring web starter
      * spock

---
+++ src/main/groovy/com/mona/search/SearchApplication.groovy
@@ -0,0 +1,12 @@
+package com.mona.search
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SearchApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SearchApplication, args)
+	}
+}
---
+++ src/test/groovy/com/mona/search/SearchApplicationTests.groovy
@@ -0,0 +1,12 @@
+package com.mona.search
+
+import spock.lang.Specification
+
+class SearchApplicationTests extends Specification {
+
+	void 'dummy test to test build/tests'() {
+        expect:
+        4 == 3 + 2
+    }
+
+}
