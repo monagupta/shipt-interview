f36c4e4278a7413892162f431faa65b5082cb915
monagupta <monagupta43@gmail.com>
2018-07-10 19:15:21 -0500

    Setup initial Spring Boot project (#1)
    Includes dependencies:
      * spring boot starter
      * spring web starter
      * spock
      * elastic search

---
+++ src/test/groovy/com/mona/search/SearchApplicationSpec.groovy
@@ -0,0 +1,12 @@
+package com.mona.search
+
+import spock.lang.Specification
+
+class SearchApplicationSpec extends Specification {
+
+    void 'dummy test to test build/tests'() {
+        expect:
+        4 == 2 + 2
+    }
+
+}
