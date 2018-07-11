437cb5fb4886524a76f9132f0c94a8ee1dfed186
Mona Gupta <monagupta43@gmail.com>
2018-07-10 19:09:31 -0500

    Fix formatting issues

--- src/main/groovy/com/mona/search/SearchApplication.groovy
+++ src/main/groovy/com/mona/search/SearchApplication.groovy
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class SearchApplication {
 
-	static void main(String[] args) {
-		SpringApplication.run(SearchApplication, args)
-	}
+    static void main(String[] args) {
+        SpringApplication.run(SearchApplication, args)
+    }
 }
--- src/test/groovy/com/mona/search/SearchApplicationSpec.groovy
+++ src/test/groovy/com/mona/search/SearchApplicationSpec.groovy
@@ -2,9 +2,9 @@ package com.mona.search
 
 import spock.lang.Specification
 
-class SearchApplicationTests extends Specification {
+class SearchApplicationSpec extends Specification {
 
-	void 'dummy test to test build/tests'() {
+    void 'dummy test to test build/tests'() {
         expect:
         4 == 2 + 2
     }
